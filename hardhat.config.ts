8803f6cc98b7cf383f51b800d1f65691377da08f
Tim Pechersky <tim.pechersky@gmail.com>
2023-11-19 23:18:54 +0000

    wip

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -129,7 +129,7 @@ export default {
     compilers: [
       {
         version: '0.8.4',
-        
+
         settings: {
           optimizer: {
             enabled: true,
@@ -191,7 +191,7 @@ export default {
   ],
   typechain: {
     outDir: 'types',
-    target: 'ethers-v5',
+    target: 'ethers-v6',
     alwaysGenerateOverloads: true, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
     // externalArtifacts: ["externalArtifacts/*.json"], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
   },
