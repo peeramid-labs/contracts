a07ebeb5641ab8bdc39118ddf15bb5ca0a362137
Tim Pechersky <tim.pechersky@gmail.com>
2024-03-07 23:59:30 +0100

    generic managed asset factory specs

---
+++ src/interfaces/IFactoryAssetsManager.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+interface IFactoryAssetsManager {
+
+    function deployAsset(bytes32 assetUri, bytes32 assetType, bytes calldata instantiationPayload) external;
+    function deployAssetManager(address sAddr, bytes32 strategyId, bytes calldata instantiationPayload) external;
+    function isAssetManager(address maybeManager) external view returns (bool);
+    function isManagedAsset(address maybeAsset) external view returns (bool);
+    function getAsset(address manager) external view returns (address);
+    function getAssetType(address asset) external view returns (bytes32);
+    function getAssetUri(address asset) external view returns (bytes32);
+
+    event AssetDeployed(address indexed asset, bytes32 indexed assetUri, bytes32 indexed assetType);
+    event AssetManagerDeployed(address indexed asset, address indexed manager, bytes32 indexed strategyId);
+
+}
+
+
