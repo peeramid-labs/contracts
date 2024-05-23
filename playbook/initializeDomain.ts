44a958d29daad6f522ca33c6e963ace30479e6de
Tim Pechersky <tim.pechersky@gmail.com>
2024-05-23 10:41:45 +0200

    fixed stuff

--- playbook/initializeDomain.ts
+++ playbook/initializeDomain.ts
@@ -7,25 +7,35 @@ task('initializeDomain', 'Just a test task')
   .addOptionalParam('fee', 'Fee  amount in base currency of network')
   .addOptionalParam('reward', 'Referral share in base currency of network')
   .addOptionalParam('discount', 'Discount in base currency of network')
-  .setAction(async ( { domain, fee, reward, discount, registrarAddress }: { domain: string; fee: string; reward: string; discount: string, registrarAddress: string }, hre) => {
-    const { deployments, getNamedAccounts } = hre
-    const { owner, registrar } = await getNamedAccounts();
-    const multipassDeployment = await deployments.get('Multipass');
-    hre.multipass = new hre.ethers.Contract(
-      multipassDeployment.address,
-      multipassDeployment.abi,
-      hre.ethers.provider.getSigner(owner),
-    ) as MultipassDiamond;
-    const tx = await hre.multipass
-      .initializeDomain(
-        registrarAddress ?? hre.ethers.provider.getSigner(registrar).address,
+  .setAction(
+    async (
+      {
+        domain,
+        fee,
+        reward,
+        discount,
+        registrarAddress,
+      }: { domain: string; fee: string; reward: string; discount: string; registrarAddress: string },
+      hre,
+    ) => {
+      const { deployments, getNamedAccounts } = hre;
+      const { owner, registrar } = await getNamedAccounts();
+      const multipassDeployment = await deployments.get('Multipass');
+      const multipassContract = new hre.ethers.Contract(
+        multipassDeployment.address,
+        multipassDeployment.abi,
+        hre.ethers.provider.getSigner(owner),
+      ) as MultipassDiamond;
+      const tx = await multipassContract.initializeDomain(
+        registrarAddress ?? registrar,
         1000,
         hre.ethers.utils.parseEther(fee ?? '0'),
         hre.ethers.utils.formatBytes32String(domain ?? 'rankify.it'),
         hre.ethers.utils.parseEther(reward ?? '0'),
         hre.ethers.utils.parseEther(discount ?? '0'),
       );
-    console.log(tx);
-  });
+      console.log(tx);
+    },
+  );
 
 export default {};
