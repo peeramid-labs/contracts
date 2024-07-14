c9351d3d2327a2b0aae2541115524323117d1d64
Tim Pechersky <tim.pechersky@gmail.com>
2024-07-14 17:31:55 +0200

    less interfaces

--- src/vendor/eds/abstracts/CodeIndexer.sol
+++ src/vendor/eds/abstracts/CodeIndexer.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.20;
 
 import "../interfaces/ICodeIndex.sol";
-import "../interfaces/IIndexUser.sol";
 
-abstract contract CodeIndexer is IIndexUser {
+abstract contract CodeIndexer  {
     ICodeIndex constant indexContract = ICodeIndex(0xC0D31d4e7987f2eD6bf5225bb28bc14a84858F42);
 
     constructor() {}
 
-    function getContractsIndex() public pure returns (ICodeIndex) {
+    function getContractsIndex() internal pure returns (ICodeIndex) {
         return indexContract;
     }
 
