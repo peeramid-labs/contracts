35954a581d28688c35e9173ca4c5dce0968ec973
Tim Pechersky <tim.pechersky@gmail.com>
2024-11-19 20:24:33 +0700

    fix scripts

--- scripts/playbooks/addDistribution.ts
+++ scripts/playbooks/addDistribution.ts
@@ -1,29 +1,25 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { ethers } from 'hardhat';
 import { Signer } from '@ethersproject/abstract-signer';
-import { PeeramidLabsDistributor } from '../../types';
+import { DAODistributor } from '../../types';
 
 export const addDistributionNoChecks =
   (hre: HardhatRuntimeEnvironment) => async (distrId: string, signer: Signer, initializer?: string) => {
     const { deployments } = hre;
-    const PeeramidLabsDistributor = await deployments.get('PeeramidLabsDistributor');
-    const distributorContract = new ethers.Contract(
-      PeeramidLabsDistributor.address,
-      PeeramidLabsDistributor.abi,
-      signer,
-    );
+    const DAODistributor = await deployments.get('DAODistributor');
+    const distributorContract = new ethers.Contract(DAODistributor.address, DAODistributor.abi, signer);
     return distributorContract.addDistribution(distrId, initializer ?? ethers.constants.AddressZero);
   };
 
 export const addDistribution =
   (hre: HardhatRuntimeEnvironment) => async (distrId: string, signer: Signer, initializer?: string) => {
     const { deployments } = hre;
-    const PeeramidLabsDistributor = await deployments.get('PeeramidLabsDistributor');
+    const DAODistributor = await deployments.get('DAODistributor');
     const distributorContract = new ethers.Contract(
-      PeeramidLabsDistributor.address,
-      PeeramidLabsDistributor.abi,
+      DAODistributor.address,
+      DAODistributor.abi,
       signer,
-    ) as PeeramidLabsDistributor;
+    ) as DAODistributor;
     const distributionsLengthBefore = (await distributorContract.getDistributions()).length;
     const receipt = await distributorContract['addDistribution(bytes32,address)'](
       distrId,
